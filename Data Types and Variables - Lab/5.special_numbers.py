4e9314b791d5b440a873a209ffb73efda1bb454f
Konstantin981 <Kok97h@gmail.com>
2025-09-16 20:58:47 +0300

    Added new exercise files from new lecture

---
+++ Data Types and Variables - Lab/5.special_numbers.py
@@ -0,0 +1,9 @@
+n = int(input())
+for i in range(1, n+1):
+    sum = 0
+    for digit in str(i):
+        sum += int(digit)
+    if sum == 5 or sum == 7  or sum == 11:
+        print(f"{i} -> True")
+    else:
+        print(f"{i} -> False")
