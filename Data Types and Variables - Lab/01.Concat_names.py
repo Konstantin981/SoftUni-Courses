329624334a87c73610809e144572d097e1ca4776
Konstantin981 <Kok97h@gmail.com>
2025-09-16 20:30:38 +0300

    Added exercise files from new topic

--- Data Types and Variables - Lab/01.Concat_names.py
+++ Data Types and Variables - Lab/01.Concat_names.py
@@ -1,1 +1,4 @@
-print("Hello World")
+first_name = input()
+last_name = input()
+delimeter = input()
+print(f"{first_name}{delimeter}{last_name}")
---
+++ Data Types and Variables - Lab/03.pounds_to_dollars.py
@@ -0,0 +1,4 @@
+pounds = int(input())
+DOLLARS_TO_POUNDS = 1.31
+dollars = pounds * DOLLARS_TO_POUNDS
+print(f"{dollars:.3f}")
