5b082779c02a3e462b8322dbacfc8475d9f7153a
Konstantin Hristov <Kok97h@gmail.com>
2025-11-23 16:00:28 +0200

    Started final exam preparation 2 with first exercise

---
+++ PythonFundamentals2025/Final Exam Preparation 2/01.secret_chat.py
@@ -0,0 +1,25 @@
+concealed_message = input()
+command = input()
+while command != "Reveal":
+    command = command.split(":|:")
+    action = command[0]
+    if action == "InsertSpace":
+        index = int(command[1])
+        concealed_message = concealed_message[:index] + " " + concealed_message[index:]
+        print(concealed_message)
+    elif action == "Reverse":
+        substring = command[1]
+        if substring in concealed_message:
+            concealed_message = concealed_message.replace(substring, "")
+            concealed_message += substring[::-1]
+            print(concealed_message)
+        else:
+            print("error")
+    elif action == "ChangeAll":
+        substring = command[1]
+        replacement = command[2]
+        while substring in concealed_message:
+            concealed_message=concealed_message.replace(substring, replacement)
+        print(concealed_message)
+    command = input()
+print(f"You have a new text message: {concealed_message}")
